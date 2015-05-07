4dcc9da4655e6a66432c2bc60a20107d01048f18
Huynh Minh Tu <hmtu.tl@gmail.com>
2015-05-07 23:25:54 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+jQuery('.company').lbSlider({
+    leftBtn: '.company .left',
+    rightBtn: '.company .right',
+    visible: 6,
+    autoPlay: true,
+    autoPlayDelay: 5
+});
+
+jQuery('.content__features').lbSlider({
+    leftBtn: '.content__features .left',
+    rightBtn: '.content__features .right',
+    visible: 3,
+    autoPlay: true,
+    autoPlayDelay: 5
+});
